d8a9f01c7286f514a86fefb3350a9d07784ef288
prajjwal w3dev <prajjwal@w3dev.email>
2021-01-08 22:48:30 +0530

    first

---
+++ mylibrary/src/main/java/com/exm/mylibrary/WebRTC/MainActivity.java
@@ -0,0 +1,18 @@
+package com.exm.mylibrary.WebRTC;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.exm.mylibrary.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("pkkkkkkkkkkk","you are a good kid");
+    }
+}
