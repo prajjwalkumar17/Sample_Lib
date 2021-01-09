23ef035df00d9a4024b7fa4cfb9cd1d39c138088
prajjwal w3dev <prajjwal@w3dev.email>
2021-01-10 02:07:51 +0530

    second

--- app/src/main/java/com/exm/lib/MainActivity.java
+++ app/src/main/java/com/exm/lib/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.exm.mylibrary.WebRTC.WebRTC;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebRTC webRTC=new WebRTC();
+        webRTC.toaster(getApplicationContext(),"hello this working correctly");
     }
 }
--- mylibrary/src/main/java/com/exm/mylibrary/WebRTC/WebRTC.java
+++ mylibrary/src/main/java/com/exm/mylibrary/WebRTC/WebRTC.java
@@ -1,7 +1,16 @@
 package com.exm.mylibrary.WebRTC;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class WebRTC {
 
+    public void toaster(Context context,String message){
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        toast.show();
+
+
+    }
 
 
 }
